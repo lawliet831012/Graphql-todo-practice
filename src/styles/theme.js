c62a6bac269fd3f2219c3be8d769199d35d1ff0f
louis wu <lawliet831012@gmail.com>
2022-01-05 13:05:44 +0800

    test pre-commit

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,7 +2,14 @@ import { createTheme } from '@mui/material/styles';
 
 // Create a theme instance.
 const theme = createTheme({
-  palette: {}
+  palette: {
+    primary: {
+      main: '#61dafb'
+    },
+    error: {
+      main: '#ff0201'
+    }
+  }
 });
 
 export default theme;
